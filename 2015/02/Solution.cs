fae98ce910d2a28e366b5a31216c7adf28c37d15
robolix11 <robolix11@gmail.com>
2020-05-07 15:53:01 +0200

    task 2015-02 setup

---
+++ 2015/02/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace AdventOfCode._2015._02
+{
+    public class Solution
+    {
+
+        private static string getInput()
+        {
+            return File.ReadAllText("..\\..\\..\\2015\\02\\Input.txt");
+        }
+
+        public static void Part1()
+        {
+
+        }
+
+        public static void Part2()
+        {
+
+        }
+    }
+}
